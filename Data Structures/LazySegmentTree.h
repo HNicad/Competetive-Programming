6e3c1d3cb9b83b4346e96243c879daa4e95b9e41
Nicad <58739441+HNicad@users.noreply.github.com>
2020-08-27 08:26:06 +0400

    Update LazySegmentTree.h

--- Data Structures/LazySegmentTree.h
+++ Data Structures/LazySegmentTree.h
@@ -26,7 +26,7 @@ class LazySegmentTree{
 		}
 		/**
 		 *   Adds val to the range [l,r] .
-		 *   @param2 l - left side of the range.
+		 *   @param1 l - left side of the range.
  		 *   @param2 r - right side of the range.
 		**/
 		void Update(int l, int r, T val){ 
@@ -34,7 +34,7 @@ class LazySegmentTree{
 		}
 		/**
 		 *   Returns sum of the values in the range [l,r].
-		 *   @param2 l - left side of the range.
+		 *   @param1 l - left side of the range.
  		 *   @param2 r - right side of the range.
  		 *   @return sum of the values in the range [l,r] 
 		**/
